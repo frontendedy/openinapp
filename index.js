e91baf68e14d914919caab49c7c24a6a6176fb9d
Lalit Kumar <lsahu0838@gmail.com>
2024-08-24 17:31:19 +0530

    Location issue fixed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors"
-import redirect  from "../redirect.controller.js";
+import redirect  from "./redirect.controller.js";
 import bodyParser from "body-parser";
 
 const app = express();
